847e48a6c19875d66b13a74c502e889d5cb56b90
sampatdhakal1999 <sampatdhakal@gmail.com>
2022-05-07 19:04:56 +0545

    added header and footer

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 let items = ["Buy Food", "Eat Food", "Cook Food"];
+let workItems = ["hi"];
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -19,14 +20,31 @@ app.get("/", function(req, res) {
   let day = today.toLocaleDateString("en-US",options);
 
 
-  res.render("list", {kindOfDay: day, newListItems: items});
+  res.render("list", {listTitle: day, newListItems: items});
 });
 
 app.post("/",function(req,res) {
   let item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
-})
+
+  if (req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+
+  } else{
+    items.push(item);
+    res.redirect("/");
+  }
+
+
+
+});
+
+app.get("/work", function(req,res){
+  res.render("list",{listTitle: "Work", newListItems: workItems});
+});
+
+
+
 
 app.listen(3000, function() {
   console.log("server started on port 3000");
