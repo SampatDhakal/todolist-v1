cde3d71dd4a2a9b5f8337fb0111d1d33758e8e92
sampatdhakal1999 <sampatdhakal@gmail.com>
2022-05-07 13:53:08 +0545

    Added css

--- app.js
+++ app.js
@@ -3,26 +3,27 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-var items = ["Buy Food", "Eat Food", "Cook Food"];
+let items = ["Buy Food", "Eat Food", "Cook Food"];
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 app.get("/", function(req, res) {
 
-  var today = new Date();
-  var options = {
+  let today = new Date();
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
 
-  var day = today.toLocaleDateString("en-US",options);
+  let day = today.toLocaleDateString("en-US",options);
 
 
   res.render("list", {kindOfDay: day, newListItems: items});
 });
 
 app.post("/",function(req,res) {
-  var item = req.body.newItem;
+  let item = req.body.newItem;
   items.push(item);
   res.redirect("/");
 })
