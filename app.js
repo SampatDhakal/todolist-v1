e101d632afb8d13db2773860ccd1f035edf49eaf
sampatdhakal1999 <sampatdhakal@gmail.com>
2022-05-07 12:26:02 +0545

    Adding post method

---
+++ app.js
@@ -0,0 +1,25 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.get("/", function(req, res) {
+
+  var today = new Date();
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US",options);
+
+
+  res.render("list", { kindOfDay: day});
+});
+
+app.listen(3000, function() {
+  console.log("server started on port 3000");
+});
