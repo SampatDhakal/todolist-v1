36c6208abce4a880049fcbd35b52eb08fdf3c52e
sampatdhakal1999 <sampatdhakal@gmail.com>
2022-05-07 12:49:22 +0545

    Added loops for adding list items

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
+var items = ["Buy Food", "Eat Food", "Cook Food"];
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", function(req, res) {
 
   var today = new Date();
@@ -17,8 +18,14 @@ app.get("/", function(req, res) {
   var day = today.toLocaleDateString("en-US",options);
 
 
-  res.render("list", { kindOfDay: day});
+  res.render("list", {kindOfDay: day, newListItems: items});
 });
+
+app.post("/",function(req,res) {
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+})
 
 app.listen(3000, function() {
   console.log("server started on port 3000");
