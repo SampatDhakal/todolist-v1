a6fa9e70949c6f0a3dc69fba7e52c2238c87cd68
sampatdhakal1999 <sampatdhakal@gmail.com>
2022-05-14 12:26:09 +0545

    Code refactored

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname +"/date.js");
 
 const app = express();
-let items = ["Buy Food", "Eat Food", "Cook Food"];
-let workItems = ["hi"];
+const items = ["Buy Food", "Eat Food", "Cook Food"];
+const workItems = [""];
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.get("/", function(req, res) {
 
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US",options);
+let day = date.getDate();
 
 
   res.render("list", {listTitle: day, newListItems: items});
